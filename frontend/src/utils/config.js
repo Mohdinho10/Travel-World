c4849f3a015e697c8777b84f6b584297991aeb57
muhammadqulatain123@gmail.com <Mohdinho10>
2024-08-09 22:00:22 +0300

    Trying to fix production URL

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL = // eslint-disable-next-line no-undef
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://travel-world-5ivr.onrender.com";
+    : "https://travel-world-qx0r.onrender.com";
