27a9ed1deade2665aeb5a057dfaf4aadab480f38
muhammadqulatain123@gmail.com <Mohdinho10>
2024-08-09 23:39:01 +0300

    changes

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,4 @@
 export const BASE_URL = // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://travel-world-wo14.onrender.com";
