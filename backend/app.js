304d1a86449a41908be1a6dce145de87b41c9415
muhammadqulatain123@gmail.com <Mohdinho10>
2024-08-09 21:50:50 +0300

    Trying to fix mongo URL

--- backend/app.js
+++ backend/app.js
@@ -60,11 +60,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 20000, // Increase timeout to 20 seconds
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("mongoose connected successfully"))
   .catch((err) => err);
 
