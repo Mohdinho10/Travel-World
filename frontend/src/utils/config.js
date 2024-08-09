7bfc852f67a9924a04544c9985221eb6f48f9293
muhammadqulatain123@gmail.com <Mohdinho10>
2024-08-09 23:15:51 +0300

    Trying to fix production URL with Railway

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL = // eslint-disable-next-line no-undef
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://travel-world-production.up.railway.app/";
+    : "https://travel-world-production.up.railway.app";
